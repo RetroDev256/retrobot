9f0c9e675636ded617dc7eade8800d779b06784f
Eric Petersen <ericpetersen3.14@gmail.com>
2025-08-08 00:49:39 -0600

    add zig highlighter

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,6 @@ let rand64: () => bigint;
 let randBool: () => boolean;
 let randFloat: () => number;
 
-let words: string[];
-let nouns: string[];
-let verbs: string[];
-let adverbs: string[];
-let adjectives: string[];
-
 function ready(client: Client) {
     console.log("Logged in as", client.user?.tag);
     for (const guild of client.guilds.cache.values()) {
@@ -54,6 +48,15 @@ async function messageCreate(message: Message) {
     const lower = message.content.toLowerCase();
     if (lower.startsWith("should i") || lower.startsWith("should we")) {
         return message.reply(randBool() ? "yes" : "no");
+    }
+
+    // Colorize zig code blocks
+    for (const block of zigCodeBlocks(message.content)) {
+        const annotated = zigAnnotate(block);
+        if (annotated.length < 1990) {
+            const formatted = "```ansi\n" + annotated + "```";
+            await message.reply(formatted);
+        }
     }
 
     // Anything after this point is a command
@@ -100,12 +103,12 @@ function simple(command: string, message: Message): string[] | undefined {
             }
             break;
         case "randhex":
-            const u64_a = rand64() & 0xffff_ffff_ffff_ffffn;
-            response.push(`Here's your random u64: 0x${u64_a.toString(16)}`);
+            const u_num_a = rand64() & 0xffff_ffff_ffff_ffffn;
+            response.push(`Here's your random u64: 0x${u_num_a.toString(16)}`);
             break;
         case "rand":
-            const u64_b = rand64() & 0xffff_ffff_ffff_ffffn;
-            response.push(`Here's your random u64: ${u64_b}`);
+            const u_num_b = rand64() & 0xffff_ffff_ffff_ffffn;
+            response.push(`Here's your random u64: ${u_num_b}`);
             break;
         case "randbit":
             const bit = randBool() ? "0" : "1";
@@ -150,8 +153,8 @@ function complex(command: string, _: Message): string[] | undefined {
 function acrCommand(cleaned: string): string[] {
     assert(cleaned.length >= 1);
 
-    const u64 = rand64() & 0xffff_ffff_ffff_ffffn;
-    if (Number(u64 % 4n) === 0) {
+    const u_num = rand64() & 0xffff_ffff_ffff_ffffn;
+    if (Number(u_num % 4n) === 0 || cleaned.length > 16) {
         return Array.from(cleaned).map((letter) => getW(letter, words));
     } else if (cleaned.length === 1) {
         return [getW(cleaned[0]!, words)];
@@ -172,14 +175,6 @@ function acrCommand(cleaned: string): string[] {
         if (inc_adverb) selection.push(getW(cleaned[j_count + 2]!, adverbs));
         return selection.slice(-cleaned.length);
     }
-}
-
-function getW(letter: string, list: string[]): string {
-    assert(letter.length == 1);
-    const choices = list.filter((word) => word.startsWith(letter));
-    const u64 = rand64() & 0xffff_ffff_ffff_ffffn;
-    const choice_idx = u64 % BigInt(choices.length);
-    return choices[Number(choice_idx)]!;
 }
 
 // For reducing some number of responses
@@ -219,20 +214,137 @@ function initializeWasm(exports: WebAssembly.Exports) {
     exports.init();
 }
 
+let words: string[][];
+let nouns: string[][];
+let verbs: string[][];
+let adverbs: string[][];
+let adjectives: string[][];
+
+// Get a random word from some starting letter
+function getW(letter: string, list: string[][]): string {
+    assert(letter.length === 1);
+    assert(letter[0] === letter[0]?.toLowerCase());
+    const letter_idx = letter.charCodeAt(0) - "a".charCodeAt(0);
+    const u_num = rand64() & 0xffff_ffff_ffff_ffffn;
+    const choice_idx = u_num % BigInt(list[letter_idx]!.length);
+    return list[letter_idx]![Number(choice_idx)]!;
+}
+
 function loadWordLists() {
     const n_text = fs.readFile("static/nouns.txt", "utf8");
     const v_text = fs.readFile("static/verbs.txt", "utf8");
     const a_text = fs.readFile("static/adverbs.txt", "utf8");
     const j_text = fs.readFile("static/adjectives.txt", "utf8");
 
-    const init_a = n_text.then((text) => (nouns = text.split("\n")));
-    const init_b = v_text.then((text) => (verbs = text.split("\n")));
-    const init_c = a_text.then((text) => (adverbs = text.split("\n")));
-    const init_d = j_text.then((text) => (adjectives = text.split("\n")));
+    const init_a = n_text.then(
+        (text) => (nouns = partitionLetter(text.split("\n")))
+    );
+    const init_b = v_text.then(
+        (text) => (verbs = partitionLetter(text.split("\n")))
+    );
+    const init_c = a_text.then(
+        (text) => (adverbs = partitionLetter(text.split("\n")))
+    );
+    const init_d = j_text.then(
+        (text) => (adjectives = partitionLetter(text.split("\n")))
+    );
 
     Promise.all([init_a, init_b, init_c, init_d]).then(() => {
-        words = [...nouns, ...verbs, ...adverbs, ...adjectives];
+        words = Array.from({ length: 26 }, () => []);
+        for (let i = 0; i < 26; i += 1) {
+            words[i] = [
+                ...(nouns[i] || []),
+                ...(verbs[i] || []),
+                ...(adverbs[i] || []),
+                ...(adjectives[i] || []),
+            ];
+        }
     });
+}
+
+function partitionLetter(word_list: string[]): string[][] {
+    const lists: string[][] = Array.from({ length: 26 }, () => []);
+    for (const word of word_list) {
+        assert(word.length !== 0);
+        assert(word[0] === word[0]?.toLowerCase());
+        const letter_idx = word.charCodeAt(0) - "a".charCodeAt(0);
+        lists[letter_idx]!.push(word);
+    }
+    return lists;
+}
+
+function zigCodeBlocks(text: string): string[] {
+    const regex = /```zig\s*([\s\S]*?)```/g;
+    const matches = text.matchAll(regex);
+    const results: string[] = [];
+
+    for (const match of matches) {
+        results.push(match[1]!);
+    }
+
+    return results;
+}
+
+function zigAnnotate(input: string): string {
+    const zig_string = /"(?:\\.|[^\n"])*"|\\\\.*/g;
+    const zig_comment = /\/\/.*/g;
+    const zig_builtin = /@\w+/g;
+    const zig_keyword =
+        /\b(?:addrspace|align|allowzero|and|anyframe|anytype|asm|break|callconv|catch|comptime|const|continue|defer|else|enum|errdefer|error|export|extern|fn|for|if|inline|noalias|noinline|nosuspend|opaque|or|orelse|packed|pub|resume|return|linksection|struct|suspend|switch|test|threadlocal|try|union|unreachable|usingnamespace|var|volatile|while)\b/g;
+    const zig_primitive =
+        /\b(?:[uif]\d+|isize|usize|bool|anyopaque|void|noreturn|type|anyerror|comptime_int|comptime_float)\b/g;
+    const zig_number =
+        /(?:-|\b)[0-9][xo+\-\wpP]*|\b(?:true|false|null|undefined)\b|'(?:\\.|[^\n'])*'/g;
+    const zig_type = /\b[A-Z]\w*/g;
+    const zig_function = /\b[a-z_]\w*(?=\()/g;
+    const zig_variable = /\b\w+/g;
+
+    const white = (a: string): string => "\x1b[37m" + a; // White
+    const red = (a: string): string => "\x1b[31m" + a; // Red
+    const yellow = (a: string): string => "\x1b[33m" + a; // Yellow
+    const green = (a: string): string => "\x1b[32m" + a; // Green
+    const cyan = (a: string): string => "\x1b[36m" + a; // Cyan
+    const blue = (a: string): string => "\x1b[34m" + a; // Blue
+    const magenta = (a: string): string => "\x1b[35m" + a; // Magenta
+
+    const zig_8 = stringMatcher(zig_variable, blue, white);
+    const zig_7 = stringMatcher(zig_function, cyan, zig_8);
+    const zig_6 = stringMatcher(zig_type, magenta, zig_7);
+    const zig_5 = stringMatcher(zig_number, yellow, zig_6);
+    const zig_4 = stringMatcher(zig_primitive, red, zig_5);
+    const zig_3 = stringMatcher(zig_keyword, green, zig_4);
+    const zig_2 = stringMatcher(zig_builtin, red, zig_3);
+    const zig_1 = stringMatcher(zig_string, yellow, zig_2);
+    const zig_0 = stringMatcher(zig_comment, white, zig_1);
+
+    return zig_0(input);
+}
+
+function stringMatcher(
+    regex: RegExp,
+    tag_fn: (match: string) => string,
+    nomatch_fn: (span: string) => string
+): (source: string) => string {
+    return (source: string): string => {
+        let last_index: number = 0;
+        let annotated: string = "";
+
+        for (const match of source.matchAll(regex)) {
+            if (match.index! > last_index) {
+                const span: string = source.slice(last_index, match.index!);
+                annotated += nomatch_fn(span);
+            }
+            annotated += tag_fn(match[0]);
+            last_index = match.index! + match[0].length;
+        }
+
+        if (last_index < source.length) {
+            const span: string = source.slice(last_index);
+            annotated += nomatch_fn(span);
+        }
+
+        return annotated;
+    };
 }
 
 // Initialize everything
