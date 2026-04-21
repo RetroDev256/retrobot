f7be34bf397804adbdffbffbbc17b0940dfc0461
Eric Petersen <ericpetersen3.14@gmail.com>
2026-04-20 22:47:18 -0600

    add run command

--- build.zig
+++ build.zig
@@ -24,6 +24,7 @@ pub fn build(b: *std.Build) !void {
     wasm.entry = .disabled;
 
     const install = b.addInstallArtifact(wasm, .{});
+    b.getInstallStep().dependOn(&install.step);
 
     // Build typescript source (including adding required libraries)
     const ts_source = try b.build_root.join(b.allocator, &.{"src/main.ts"});
@@ -40,33 +41,28 @@ pub fn build(b: *std.Build) !void {
 
     // The bun build step depends on some external JS
     b.getInstallStep().dependOn(&bun_build.step);
-    for (@as([]const []const u8, &.{
-        "@google/generative-ai",
-        "discord.js",
-        "ollama",
-    })) |dependency| {
+    for (@as([]const []const u8, &.{ "discord.js", "ollama" })) |dependency| {
         bun_build.step.dependOn(&b.addSystemCommand(
             &.{ "bun", "add", dependency, "--silent" },
         ).step);
     }
 
     // The optimization of the wasm file depends on the program wasm-opt
-    const wasm_path = b.pathJoin(&.{ b.install_path, "retrobot.wasm" });
-    install.step.dependOn(&b.addSystemCommand(&.{
-        "wasm-opt",
-        "-O4",
-        "--enable-bulk-memory-opt",
-        "--enable-nontrapping-float-to-int",
-        wasm_path,
-        "-o",
-        wasm_path,
-    }).step);
+    const wasm_path = b.pathJoin(&.{ b.exe_dir, "retrobot.wasm" });
+    const optimize_wasm = b.addSystemCommand(&.{
+        "wasm-opt", b.fmt("-o={s}", .{wasm_path}),
+        "-O4",      "--enable-nontrapping-float-to-int",
+        wasm_path,  "--enable-bulk-memory-opt",
+    });
+
+    optimize_wasm.step.dependOn(&install.step);
+    b.getInstallStep().dependOn(&optimize_wasm.step);
 
     // Copy .env and "words.txt" to output directory on install step
     for (@as([]const []const u8, &.{ ".env", "words.txt" })) |dependency| {
         const path = try b.build_root.join(b.allocator, &.{dependency});
-        const copy_step = b.addSystemCommand(&.{ "cp", path, b.exe_dir });
-        install.step.dependOn(&copy_step.step);
+        const copy_command = b.addSystemCommand(&.{ "cp", path, b.exe_dir });
+        install.step.dependOn(&copy_command.step);
     }
 
     // The clean subcommand command will delete a bunch of stuff
@@ -81,4 +77,13 @@ pub fn build(b: *std.Build) !void {
         const path = try b.build_root.join(b.allocator, &.{to_go});
         clean.dependOn(&b.addSystemCommand(&.{ "rm", "-rf", path }).step);
     }
+
+    // The run subcommand will compile the wasm and run the typescript with bun
+    const run = b.step("run", "compile everything and run with bun");
+    const js_out_path = b.pathJoin(&.{ b.exe_dir, "main.js" });
+    const bun_run_command = b.addSystemCommand(
+        &.{ "env", "-C", b.exe_dir, "bun", "run", js_out_path },
+    );
+    run.dependOn(&bun_run_command.step);
+    run.dependOn(b.getInstallStep());
 }
