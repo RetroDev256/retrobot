b977cc6790d48a10ae0002f428f22b42ed2b0f79
Eric Petersen <ericpetersen3.14@gmail.com>
2026-05-02 00:32:27 -0600

    fix escape

--- src/main.ts
+++ src/main.ts
@@ -348,7 +348,7 @@ class ChunkedReplyWriter {
         // Ensure that the buffer gets the updated text appended
         this.buffer += chunk;
         // Ensure we don't get issues with formatting code
-        this.buffer.replace(/```/g, "\`\`\`");
+        this.buffer.replace(/```/g, "\\`\\`\\`");
         // A change has been made to the buffer, we are dirty
         this.dirty = true;
         // Don't update concurrently - we would be ratelimited
